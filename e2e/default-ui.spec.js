e9c48e5a6cc3207cd34b8e6f37e7efa93c6d1214
Georgios Kartalis <gkartalis@Georgioss-MacBook-Pro.local>
2019-11-28 17:52:09 +0200

    added tests for default ui

--- e2e/default-ui.spec.js
+++ e2e/default-ui.spec.js
@@ -1,17 +1,19 @@
-describe('<App />', () => {
+describe('Default UI', () => {
   beforeEach(async () => {
     await device.reloadReactNative();
   });
 
-  // it('should render two default ToDo items', async () => {
+  it('should render an add button for adding todo items', async () => {
+    await expect(element(by.id('addToDo'))).toBeVisible();
+  });
 
-  // });
+  it('should render two default ToDo items', async () => {
+    await expect(element(by.id('ToDoItem')).length).toBe(2);
+  });
 
-  // it('should render an add button for adding todo items', async () => {
-
-  // });
-
-  // it('each todo item should render a DELETE button', () => {
-
-  // });
+  it('each todo item should render a DELETE button', async () => {
+    await expect(
+      element(by.id('ToDoItem').withDescendant(by.id('Delete-Button'))),
+    ).toBeVisible();
+  });
 });
