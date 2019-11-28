befa66f197d3f53c0c63b78ee457e78d0eab0147
Georgios Kartalis <gkartalis@Georgioss-MacBook-Pro.local>
2019-11-28 17:34:24 +0200

    added default todo list

--- src/utils/todo.js
+++ src/utils/todo.js
@@ -1,5 +1,4 @@
 export default [
-  {id: 1, text: "Clean the house", createdAt: 1574529742281},
-  {id: 2, text: "Learn detox", createdAt: 1574529742282},
-  {id: 3, text: "Learn react-native-testing-library", createdAt: 1574529742283},
+  {id: 1, text: 'Clean the house', createdAt: 1574529742281},
+  {id: 2, text: 'Learn detox', createdAt: 1574529742282},
 ];
